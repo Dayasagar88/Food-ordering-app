e62d323fc0bf2eeb7a9209a440fec1ab1bdc45a5
Daya Sagar <152199702+Dayasagar88@users.noreply.github.com>
2024-08-28 20:35:50 +0530

    bug fix

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,7 @@ class About extends Component {
   constructor(props) {
     super(props);
 
-    // console.log("Parent Constructor");
+    // console.log("Parent Constructor"); 
   }
 
   componentDidMount() {
@@ -20,7 +20,7 @@ class About extends Component {
 
   render() {
     // console.log("Parent Render");
-    return (
+    return ( 
       <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32 h-[100vh]">
         <img
           src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
